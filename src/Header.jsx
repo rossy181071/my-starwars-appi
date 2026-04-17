c9f941118327c1da11097cc6a7fb9d13262d0c1f
Rossy Lopez <rossy181071@hotmail.com>
2026-04-17 17:43:48 -0600

    initial commit

---
+++ src/Header.jsx
@@ -0,0 +1,18 @@
+import Header from "../../React/bicicletas-react-app/src/Header"
+
+function Header(){
+
+    return(
+        <header>
+            <h1>My website</h1>
+
+
+        </header>
+
+
+
+    );
+
+}
+
+export default Header
