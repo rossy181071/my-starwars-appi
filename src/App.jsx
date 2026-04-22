4783ca4116af288a79abcc763b7764ed1e78952e
Rossy Lopez <rossy181071@hotmail.com>
2026-04-21 18:34:52 -0600

    Created page App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import Header from'./Header.jsx'
+import Footer from './Footer.jsx'
+import Food from './Food.jsx'
 
 function App() {
 
   return(
-    <Header/>
-
+    <>
+      <Header/>
+      <Food/>
+      <Footer/>
+      
+    </>
      
   );
   
